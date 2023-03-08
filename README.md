ccc43c949827e7fd116884df9674e0381a80988b
Lonely-Dark <vlad10game@gmail.com>
2023-03-08 10:13:34 +0300

    GLOBAL UPDATE
    Poetry dependencies, new class, .env support, asyncio support, uvloop only for Linux
    Readme.md update

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
-# Install:
-Install python 3.7.3. 
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+![Linux](https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black)
+![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
 
-For install, required modules: random,requests and time
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
 
-In the bash(or cmd) type: python -m pip install module1 module2 module3
+![GitHub repo size](https://img.shields.io/github/repo-size/Lonely-Dark/status-bot?style=flat-square)
+![GitHub](https://img.shields.io/github/license/Lonely-Dark/Phoenix-Project?style=flat-square)
 
-# Options:
-Set token vk on line 23
+# Status Bot for VK
+This is a simple bot written in Python that updates the user's VK status message periodically. The bot uses the VK API to authenticate the user and update the status message.
 
-Change time.sleep(sec) how much is needed
+## Installation
+To install the bot, you will need to have Python 3.10 or higher installed on your machine.
 
-Change text in status on line 45
+1. Clone the repository:
+`git clone https://github.com/Lonely-Dark/status-bot.git`
+
+2. Install the dependencies using poetry:
+`cd status-bot`
+`poetry update`
+`poetry lock`
+
+3. Create a .env file in the project directory and set the following environment variables:
+`token = "your_token_here"`
+## Usage
+1. To run the bot, activate the virtual environment:
+`poetry shell`
+
+2. Then run the bot:
+`python src/status.py`
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ src/status.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+# ---Lonely_Dark---
+# Python 3.10
+
+import asyncio
+import os
+import time
+
+import aiohttp
+import dotenv
+import helperclass
+from json import loads
+
+import uvloop
+
+
+class MyVk:
+    def __init__(self):
+        """
+        Class for simple use vk api
+        """
+
+        self.token = dotenv.dotenv_values(f"{os.getcwd()}/.env")['token']
+        self.session = aiohttp.ClientSession()
+        self.helper = helperclass.Helper()
+
+    async def get_all_friends(self) -> list:
+        """
+        Get all friends
+        :return: list of friends
+        """
+
+        url = 'https://api.vk.com/method/friends.get'
+        params = {'access_token': self.token, 'v': '5.131'}
+        async with self.session.post(url, data=params) as response:
+            data = loads(await response.text())
+            self.helper.logger.debug(data)
+            return data['response']['items']
+
+    async def get_all_online_friends(self) -> list:
+        """
+        Get all online friends
+        :return: list of online friends
+        """
+
+        url = 'https://api.vk.com/method/friends.getOnline'
+        params = {'access_token': self.token, 'v': '5.131'}
+        async with self.session.post(url, data=params) as response:
+            data = loads(await response.text())
+            self.helper.logger.debug(data)
+            return data['response']
+
+    async def get_recent_friend(self) -> str:
+        """
+        Get recent friends
+        :return: list of recent friends
+        """
+
+        url = 'https://api.vk.com/method/friends.getRecent'
+        params = {'access_token': self.token, 'v': '5.131', 'count': 1}
+        async with self.session.post(url, data=params) as response:
+            data = loads(await response.text())
+            self.helper.logger.debug(data)
+            return data['response'][0]
+
+    async def get_user_info(self, user_id: str) -> str:
+        """
+        Get user info
+        :param user_id: user id
+        :return: user info
+        """
+
+        url = 'https://api.vk.com/method/users.get'
+        params = {'access_token': self.token, 'v': '5.131', 'user_ids': user_id}
+        async with self.session.post(url, data=params) as response:
+            data = loads(await response.text())
+            return f"{data['response'][0]['first_name']} {data['response'][0]['last_name']}"
+
+    async def set_status(self, text: str) -> None:
+        """
+        Set status from text
+        :param text: text to set
+        :return: None
+        """
+
+        url = 'https://api.vk.com/method/status.set'
+        params = {'access_token': self.token, 'v': '5.131', 'text': text}
+        async with self.session.post(url, data=params) as response:
+            data = loads(await response.text())
+            self.helper.logger.debug(data)
+
+
+async def main():
+    my_vk = MyVk()
+
+    while True:
+        online_friends = await my_vk.get_all_online_friends()
+        await asyncio.sleep(1)
+
+        recent_friend = await my_vk.get_recent_friend()
+        await asyncio.sleep(1)
+
+        user_info = await my_vk.get_user_info(recent_friend)
+        await asyncio.sleep(1)
+
+        time_is = time.localtime()
+        time_is_str = time.strftime("%d/%m/%Y", time_is)
+
+        text = f"Friends online: {len(online_friends)} \n Last application from: {user_info} \n Today is {time_is_str}"
+
+        status = await my_vk.set_status(text)
+
+        await asyncio.sleep(2)
+
+
+if __name__ == '__main__':
+    uvloop.install()
+    asyncio.run(main())
